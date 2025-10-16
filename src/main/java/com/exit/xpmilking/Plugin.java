65d11f3699a1df3b88b234c4c8049026ec50b867
A Random Guy <40922694+pseudo-Random-89@users.noreply.github.com>
2025-10-16 16:13:13 -0700

    Switched to PlayerInteractEntityEvent instead of PlayerInteractEvent

--- src/main/java/com/exit/xpmilking/Plugin.java
+++ src/main/java/com/exit/xpmilking/Plugin.java
@@ -1,24 +1,21 @@
 package com.exit.xpmilking;
 
-import java.util.Set;
 import java.util.logging.Logger;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemFlag;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.RayTraceResult;
 
 /*
  * xp-milking java plugin
  */
 public class Plugin extends JavaPlugin implements Listener {
 	private static final Logger LOGGER=Logger.getLogger("xp-milking");
+	private static boolean halver = false;
 
 	public void onEnable() {
 		LOGGER.info("xp-milking enabled");
@@ -31,39 +28,35 @@ public class Plugin extends JavaPlugin implements Listener {
 	}
 
 	@EventHandler
-	public void onPlayerRightClick(PlayerInteractEvent event) {
+	public void onPlayerRightClick(PlayerInteractEntityEvent event) {
+		if (halver) {
+			halver = false; 
+			return;
+		}
+
 		Player player = event.getPlayer();
+		ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
 
-		if (event.getItem() == null) return;
+		boolean isHoldingGlassBottle = item.getType().toString().equals("GLASS_BOTTLE");
 
-		boolean isHoldingGlassBottle = event.getItem().getType().toString().equals("GLASS_BOTTLE");
-
-		if (!isHoldingGlassBottle || (!event.getAction().toString().equals("RIGHT_CLICK_AIR") && !event.getAction().toString().equals("RIGHT_CLICK_BLOCK"))) return;
-
-		LOGGER.info(player.getDisplayName() + " right clicked");
-		player.sendMessage("You right-clicked");
-
-		RayTraceResult res = player.rayTraceBlocks(10);
-
-		// If another player was not right-clicked on, return
-		if (res == null || res.getHitEntity() == null || !(res.getHitEntity() instanceof Player)) return;
+		if (!isHoldingGlassBottle) return;
 		
-		Player tgt = (Player) res.getHitEntity();
-		player.sendMessage("You right-clicked " + tgt.getDisplayName());
-		LOGGER.info(player.getDisplayName() + " right clicked " + tgt.getDisplayName());
-
-		LOGGER.info(player.getDisplayName() + " has milked " + tgt.getDisplayName());
-		int targetXP = tgt.getTotalExperience();
+		Player tgt = (Player) event.getRightClicked();
+		float targetXP = tgt.getTotalExperience();
 
 		if (targetXP < 20) return;
 
-		tgt.setTotalExperience(targetXP - 20);
-		event.getItem().setAmount(event.getItem().getAmount() - 1);
+		player.sendMessage("You milked " + tgt.getDisplayName());
+		tgt.sendMessage(player.getDisplayName() + " milked you");
+		LOGGER.info(player.getDisplayName() + " has milked " + tgt.getDisplayName());
+
+		tgt.giveExp(-11);
+		item.setAmount(item.getAmount() - 1);
+
 		ItemStack xpBottle = new ItemStack(Material.EXPERIENCE_BOTTLE, 1);
 		player.getInventory().addItem(xpBottle);
+
+		halver = true;
 		// Any other actions upon successful milking
 	}
 }
-
-
-// Consumes 20 XP and produces 1 Bottle O' Enchanting
